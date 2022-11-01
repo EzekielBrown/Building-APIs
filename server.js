9c07f24311a6e0b1d265636b496e87bc46b9e69e
Zek Brown <zek-brown@live.nmit.ac.nz>
2022-11-01 19:06:37 +1300

    added nodemon

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const port = 3000;
 // Middleware
 app.use(express.static(path.join(__dirname, './view')))
 
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './view/index.html'));
-    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './view/category1.html'));
-    })
+});
 
+// Server
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-    });
-
+});
