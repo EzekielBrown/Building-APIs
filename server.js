54dad4704bef7da7f4d9460da779c98f4b186e56
Zek Brown <zek-brown@live.nmit.ac.nz>
2022-11-04 11:36:31 +1300

    fixed css, added routes

--- server.js
+++ server.js
@@ -2,19 +2,60 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || "3000";
+
+const users = [
+    { name: "Kevin", age: 25 },
+    { name: "John", age: 30 },
+    { name: "Jane", age: 20 }
+];
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
 
 // Middleware
-app.use(express.static(path.join(__dirname, './view')))
+app.use(express.static(path.join(__dirname, './views')));
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './view/index.html'));
+app.get('/users', (req, res) => {
+    res.status(200).send(users);
+});
+
+app.get('/users/:name', (req, res) => {
+    const { name } = req.params;
+    const user = users.find(user => user.name === name);
+    if (user) res.status(200).send(user);
+    else res.status(404).send('User not found');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './view/category1.html'));
-});
+    res.status(200).render('index', { users });
+})
+
+app.get('/login' , (req, res) => {
+    res.status(200).render('login');
+})
+
+app.get('/cart' , (req, res) => {
+    res.status(200).render('cart');
+})
+
+app.get('/category1' , (req, res) => {
+    res.status(200).render('category1');
+})
+
+app.get('/category2' , (req, res) => {
+    res.status(200).render('category2');
+})
+
+app.get('/category3' , (req, res) => {
+    res.status(200).render('category3');
+})
+
+app.get('/category4' , (req, res) => {
+    res.status(200).render('category4');
+})
 
 // Server
 app.listen(port, () => {
